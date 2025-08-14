64db809c55da13b5081642ec4d9c4d620530ca08
Zafar205 <mz09206@st.habib.edu.pk>
2025-08-15 00:03:56 +0500

    basic board created

---
+++ src/components/ColumnContainer.tsx
@@ -0,0 +1,28 @@
+import type { Column, ID } from "../types"
+import { Trash2 } from "lucide-react"
+interface Props {
+    column: Column,
+    deleteColumn: (id: ID) => void
+}
+
+const ColumnContainer = (props: Props) => {
+    const { column, deleteColumn } = props;
+
+    return (
+        <div className="bg-gray-800 w-[350px] h-[500px] max-h-[500px] rounded-md flex flex-col">
+            <div className="bg-gray-900 text-md h-[60px] cursor-grab rounded-md rounded-b-none p-3 font-bold border-gray-800 border-4 flex items-center justify-between">
+                <div className="flex gap-2">
+                    <div className="flex justify-center items-center bg-gray-800 px-2 py-1 text-sm rounded-full">0</div>
+                    <div className="py-[2px]">{column.title}</div>
+                </div>
+                <div onClick={() => deleteColumn(column.id)}>
+                    <Trash2 className="hover:bg-gray-600" />
+                </div>
+            </div>
+            <div className="flex flex-grow">Content</div>
+            <div>footer</div>
+        </div>
+    )
+}
+
+export default ColumnContainer
--- src/components/Kanban.tsx
+++ src/components/Kanban.tsx
@@ -1,14 +1,42 @@
-import {Plus} from 'lucide-react';
+import {  Plus } from 'lucide-react';
+import { useState } from 'react';
+import type { Column, ID } from '../types';
+import ColumnContainer from './ColumnContainer';
 
 const Kanban = () => {
+  const [columns, setColumns] = useState<Column[]>([])
+
+  console.log(columns)
+
+  function generateId() {
+    return `${Date.now()}-${Math.random().toString(36).slice(2, 11)}`
+  }
+
+  function createNewColumn() {
+    const columnToAdd: Column = {
+      id: generateId(),
+      title: `Column ${columns.length + 1}`
+    }
+    setColumns([...columns, columnToAdd])
+  }
+
+  function deleteColumn(id: ID){
+    const filteredColumns = columns.filter(col => col.id !== id)
+    setColumns(filteredColumns);
+  }
   return (
     <div className="m-auto flex min-h-screen w-full items-center overflow-x-auto overflow-y-hidden px-[40px]">
-        <div className="m-auto">
-                <button className="h-[60px] w-[350px] min-w-[350px] cursor-pointer rounded-lg bg-gray-900 border-2 border-gray-800 p-4 ring-rose-500 hover:ring-2 flex items-center justify-center gap-2 text-white">
-                  <Plus className="w-5 h-5 text-white" />
-                  Add Column
-                </button>
+      <div className="m-auto flex gap-4">
+        <div className='flex gap-4'>
+          {columns.map((col) => (
+            <ColumnContainer key={col.id} column={col} deleteColumn={deleteColumn} />
+          ))}
         </div>
+        <button onClick={() => { createNewColumn() }} className="h-[60px] w-[350px] min-w-[350px] cursor-pointer rounded-lg bg-gray-900 border-2 border-gray-800 p-4 ring-rose-500 hover:ring-2 flex items-center justify-center gap-2 text-white">
+          <Plus className="w-5 h-5 text-white" />
+          Add Column
+        </button>
+      </div>
     </div>
   )
 }
