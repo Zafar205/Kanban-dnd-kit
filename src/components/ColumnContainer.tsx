8a2b2519b4bc5eac541e1af29d60ad1104351b58
Zafar205 <mz09206@st.habib.edu.pk>
2025-08-15 01:44:44 +0500

    more changes

--- src/components/ColumnContainer.tsx
+++ src/components/ColumnContainer.tsx
@@ -1,4 +1,6 @@
 import type { Column, ID } from "../types"
+import { useSortable } from "@dnd-kit/sortable"
+import {CSS} from "@dnd-kit/utilities"
 import { Trash2 } from "lucide-react"
 interface Props {
     column: Column,
@@ -8,9 +10,28 @@ interface Props {
 const ColumnContainer = (props: Props) => {
     const { column, deleteColumn } = props;
 
+    const {setNodeRef, attributes, listeners, transform, transition, isDragging} = useSortable({
+        id : column.id,
+        data : {
+            type : "Column",
+            column
+        }
+    })
+
+    const style = {
+        transition,
+        transform: CSS.Transform.toString(transform)
+    }
+    if(isDragging){
+        return (
+            <div ref={setNodeRef} style={style} className="bg-gray-800 w-[350px] h-[500px] opacity-40 border-2 border-rose-500 max-h-[500px] rounded-md flex flex-col">
+            </div>
+        )
+    }
+
     return (
-        <div className="bg-gray-800 w-[350px] h-[500px] max-h-[500px] rounded-md flex flex-col">
-            <div className="bg-gray-900 text-md h-[60px] cursor-grab rounded-md rounded-b-none p-3 font-bold border-gray-800 border-4 flex items-center justify-between">
+        <div ref = {setNodeRef} style = {style} className="bg-gray-800 w-[350px] h-[500px] max-h-[500px] rounded-md flex flex-col">
+            <div {...attributes} {...listeners} className="bg-gray-900 text-md h-[60px] cursor-grab rounded-md rounded-b-none p-3 font-bold border-gray-800 border-4 flex items-center justify-between">                
                 <div className="flex gap-2">
                     <div className="flex justify-center items-center bg-gray-800 px-2 py-1 text-sm rounded-full">0</div>
                     <div className="py-[2px]">{column.title}</div>
@@ -19,6 +40,7 @@ const ColumnContainer = (props: Props) => {
                     <Trash2 className="hover:bg-gray-600" />
                 </div>
             </div>
+            
             <div className="flex flex-grow">Content</div>
             <div>footer</div>
         </div>
--- src/components/Kanban.tsx
+++ src/components/Kanban.tsx
@@ -1,12 +1,18 @@
 import {  Plus } from 'lucide-react';
-import { useState } from 'react';
+import { useMemo, useState } from 'react';
 import type { Column, ID } from '../types';
 import ColumnContainer from './ColumnContainer';
+import { DndContext, DragOverlay, type DragStartEvent } from '@dnd-kit/core';
+import { SortableContext } from '@dnd-kit/sortable';
+import { createPortal } from 'react-dom';
 
 const Kanban = () => {
   const [columns, setColumns] = useState<Column[]>([])
+  const [activeColumn, setActiveColumn] = useState<Column | null>()
 
-  console.log(columns)
+  //array of colum ids
+  const columnsId = useMemo(()=> columns.map((col)=>col.id), [columns])
+  // console.log(columns)
 
   function generateId() {
     return `${Date.now()}-${Math.random().toString(36).slice(2, 11)}`
@@ -24,19 +30,41 @@ const Kanban = () => {
     const filteredColumns = columns.filter(col => col.id !== id)
     setColumns(filteredColumns);
   }
+
+  function onDragStart(event : DragStartEvent){
+    // console.log("Drag Start", event)
+    if (event.active.data.current?.type === "Column"){
+      setActiveColumn(event.active.data.current?.column);
+      return;
+    }
+  }
   return (
     <div className="m-auto flex min-h-screen w-full items-center overflow-x-auto overflow-y-hidden px-[40px]">
+      <DndContext onDragStart={onDragStart}>
       <div className="m-auto flex gap-4">
+
         <div className='flex gap-4'>
+          <SortableContext items={columnsId}>
           {columns.map((col) => (
             <ColumnContainer key={col.id} column={col} deleteColumn={deleteColumn} />
           ))}
+          </SortableContext>
         </div>
         <button onClick={() => { createNewColumn() }} className="h-[60px] w-[350px] min-w-[350px] cursor-pointer rounded-lg bg-gray-900 border-2 border-gray-800 p-4 ring-rose-500 hover:ring-2 flex items-center justify-center gap-2 text-white">
           <Plus className="w-5 h-5 text-white" />
           Add Column
         </button>
       </div>
+      {
+        createPortal(
+          <DragOverlay>
+            {activeColumn && (
+              <ColumnContainer deleteColumn={deleteColumn} column={activeColumn} />
+            )}
+          </DragOverlay>, document.body
+        )
+      }
+      </DndContext>
     </div>
   )
 }
